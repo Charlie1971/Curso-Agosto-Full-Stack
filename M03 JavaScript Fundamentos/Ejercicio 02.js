4e6c81cf246437dc20b3f28952999a3bac719811
Carlos Ibarguren <cjibarguren@icloud.com>
2023-08-08 21:05:57 -0300

    ALGUN COMANDO

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,7 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,16 +12,19 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   return x + y;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
-}
+   return x-y;
+   }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   return x / y;
 }
 
 function multiplica(x, y) {
@@ -31,6 +35,7 @@ function multiplica(x, y) {
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
